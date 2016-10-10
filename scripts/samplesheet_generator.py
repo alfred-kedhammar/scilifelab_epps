b33567496eb9f9009264f62f15ef41bd45b8b8bd
Denis Moreno <denis.moreno@scilifelab.se>
2016-10-10 15:15:10 +0200

    fixing the fix

--- scripts/samplesheet_generator.py
+++ scripts/samplesheet_generator.py
@@ -71,7 +71,7 @@ def gen_X_lane_data(pro):
             sp_obj['sid'] = "Sample_{}".format(sample.name)
             sp_obj['sn'] = sample.name
             sp_obj['pj'] = sample.project.name
-            sp_obj['fc'] = inp.location[0].name
+            sp_obj['fc'] = io[1]['uri'].location[0].name
             sp_obj['sw'] = inp.location[1]
             idxs = find_barcode(sample, pro)
             sp_obj['idx1'] = idxs[0]
