f788f0c2beb88fa31e10ed17526908db27ae8b9f
alfred-kedhammar <alfred.kedhammar@scilifelab.se>
2022-01-11 16:06:48 +0100

    In log, list samples numerically

--- scripts/bravo_csv.py
+++ scripts/bravo_csv.py
@@ -519,7 +519,7 @@ def zika_vols(samples, target_pool_vol, target_pool_conc, pool_name, log,
 
     # Set any negative concentrations to 0.01 nM
     if not df.loc[df.conc < 0.01, "conc"].empty:
-        neg_conc_sample_names = df.loc[df.conc < 0.01, "name"]
+        neg_conc_sample_names = df.loc[df.conc < 0.01, "name"].sort_values()
         df.loc[df.conc < 0.01, "conc"] = 0.01
         log.append("WARNING: The following samples had a negative concentration and will be treated as 0.01 nM: {}".format(", ".join(neg_conc_sample_names)))
 
@@ -527,7 +527,7 @@ def zika_vols(samples, target_pool_vol, target_pool_conc, pool_name, log,
     df = df.rename(columns = {"vol":"full_vol"})
     df["live_vol"] = df.full_vol - src_dead_vol
     if any(df.live_vol < zika_min_vol):
-        log.append("ERROR: The following sample(s) did not have enough recorded volume to be transferred: {}".format(", ".join(df[df.live_vol < zika_min_vol].name)))
+        log.append("ERROR: The following sample(s) did not have enough recorded volume to be transferred: {}".format(", ".join(df[df.live_vol < zika_min_vol].sort_values("name").name)))
         raise LowVolume()
 
     # Determine lowest / highest common transfer amount
