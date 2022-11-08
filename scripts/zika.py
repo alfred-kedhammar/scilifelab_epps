4508190e5db490a76bc3d7ee7b1b4246a22559e6
kedhammar <alfred.kedhammar@scilifelab.se>
2022-11-08 10:28:51 +0100

    Black formatting and docstrings

--- scripts/zika.py
+++ scripts/zika.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+DESC = """
+This module contains functions used to generate worklists for the Mosquito X1
+instrument Zika using sample data fetched from Illumina Clarity LIMS.
+
+The functions are written with the intention of being re-useable for different
+applications of the instrument.
+
+Written by Alfred Kedhammar
+"""
+
 import pandas as pd
 import numpy as np
 from datetime import datetime as dt
@@ -8,23 +18,23 @@ from genologics.config import BASEURI, USERNAME, PASSWORD
 from genologics.entities import Process
 
 
-
 def verify_step(lims, currentStep, target_instrument, target_workflow, target_step):
-    """ Verify the instrument, workflow and step for a given process is correct """
-    
-    workflows = [art.workflow_stages[0].workflow.name for art in currentStep.all_inputs()]
-    
+    """Verify the instrument, workflow and step for a given process is correct"""
+
+    workflows = [
+        art.workflow_stages[0].workflow.name for art in currentStep.all_inputs()
+    ]
+
     verify_bools = [
-        currentStep.instrument.name == target_instrument, 
-        currentStep.type.name == target_step, 
-        all([wf == target_workflow for wf in workflows])
+        currentStep.instrument.name == target_instrument,
+        currentStep.type.name == target_step,
+        all([wf == target_workflow for wf in workflows]),
     ]
 
     if all(verify_bools):
         return True
     else:
         return False
-
 
 
 def fetch_sample_data(currentStep, to_fetch):
@@ -38,52 +48,59 @@ def fetch_sample_data(currentStep, to_fetch):
     """
 
     key2expr = {
-        "sample_name" :     "art_tuple[0]['uri'].name",
-        "source_fc"  :      "art_tuple[0]['uri'].location[0].name",
-        "source_well" :     "art_tuple[0]['uri'].location[1]",
-        "conc_units" :      "art_tuple[0]['uri'].samples[0].artifact.udf['Conc. Units']",
-        "conc" :            "art_tuple[0]['uri'].samples[0].artifact.udf['Concentration']",
-        "vol" :             "art_tuple[0]['uri'].samples[0].artifact.udf['Volume (ul)']",
-        "amt" :             "art_tuple[0]['uri'].samples[0].artifact.udf['Amount (ng)']",
-        "dest_fc" :         "art_tuple[1]['uri'].location[0].id",
-        "dest_well" :       "art_tuple[1]['uri'].location[1]",
-        "dest_fc_name" :    "art_tuple[1]['uri'].location[0].name",
-        "target_vol" :      "art_tuple[1]['uri'].udf['Total Volume (uL)']",
-        "target_amt" :      "art_tuple[1]['uri'].udf['Amount taken (ng)']"
-        }
-    
-    assert all([k in key2expr.keys() for k in to_fetch])
+        "sample_name": "art_tuple[0]['uri'].name",
+        "source_fc": "art_tuple[0]['uri'].location[0].name",
+        "source_well": "art_tuple[0]['uri'].location[1]",
+        "conc_units": "art_tuple[0]['uri'].samples[0].artifact.udf['Conc. Units']",
+        "conc": "art_tuple[0]['uri'].samples[0].artifact.udf['Concentration']",
+        "vol": "art_tuple[0]['uri'].samples[0].artifact.udf['Volume (ul)']",
+        "amt": "art_tuple[0]['uri'].samples[0].artifact.udf['Amount (ng)']",
+        "dest_fc": "art_tuple[1]['uri'].location[0].id",
+        "dest_well": "art_tuple[1]['uri'].location[1]",
+        "dest_fc_name": "art_tuple[1]['uri'].location[0].name",
+        "target_vol": "art_tuple[1]['uri'].udf['Total Volume (uL)']",
+        "target_amt": "art_tuple[1]['uri'].udf['Amount taken (ng)']",
+    }
+
+    assert all(
+        [k in key2expr.keys() for k in to_fetch]
+    ), "fetch_sample_data() did not recognize key"
 
     l = []
-    art_tuples = [art_tuple for art_tuple in currentStep.input_output_maps if art_tuple[0]["uri"].type == art_tuple[1]["uri"].type == "Analyte"]
-    for art_tuple in art_tuples:
+    art_tuples = [
+        art_tuple
+        for art_tuple in currentStep.input_output_maps
+        if art_tuple[0]["uri"].type == art_tuple[1]["uri"].type == "Analyte"
+    ]
 
+    for art_tuple in art_tuples:
         key2val = {}
-        for k in key2expr:
-            if k in to_fetch:
-                key2val[k] = eval(key2expr[k])
+        for k in to_fetch:
+            key2val[k] = eval(key2expr[k])
 
         l.append(key2val)
-    
+
     df = pd.DataFrame(l)
 
     return df
 
 
-
 def format_worklist(df, deck, buffer_strategy):
+    """
+    Pivot buffer transfers and add columns in Mosquito-intepretable format.
+    """
 
     # Pivot transfers to elucidate buffer transfers and remove zero-volume transfers
     df = pivot_buffer_transfers(df, buffer_strategy)
-    
+
     # Add columns for plate positions
-    df["src_pos"] = df["source_fc"].apply(lambda x : deck[x])
-    df["dst_pos"] = df["dest_fc"].apply(lambda x : deck[x])
-    
+    df["src_pos"] = df["source_fc"].apply(lambda x: deck[x])
+    df["dst_pos"] = df["dest_fc"].apply(lambda x: deck[x])
+
     # Convert volumes to whole nl
     df["transfer_vol"] = round(df.transfer_vol * 1000, 0)
     df["transfer_vol"] = df["transfer_vol"].astype(int)
-    
+
     # Convert well names to r/c coordinates
     df["src_row"], df["src_col"] = well2rowcol(df.source_well)
     df["dst_row"], df["dst_col"] = well2rowcol(df.dest_well)
@@ -91,54 +108,60 @@ def format_worklist(df, deck, buffer_strategy):
     return df
 
 
-
 def pivot_buffer_transfers(df, buffer_strategy):
+    """
+    Melt buffer and sample information onto separate rows to
+    produce a "one row <-> one transfer" dataframe.
+    """
 
     # Pivot buffer transfers
     to_pivot = ["sample", "buffer"]
     to_keep = ["source_fc", "source_well", "dest_fc", "dest_well"]
-    df = df.melt(value_vars = to_pivot,
-            var_name = "src_type", 
-            value_name = "transfer_vol", 
-            id_vars = to_keep).\
-                sort_values(by=["dest_well","src_type"])
+    df = df.melt(
+        value_vars=to_pivot,
+        var_name="src_type",
+        value_name="transfer_vol",
+        id_vars=to_keep,
+    ).sort_values(by=["dest_well", "src_type"])
 
     # Remove zero-vol transfers
     df = df[df.transfer_vol > 0]
 
     # Re-set index
-    df = df.reset_index(drop = True)
-    
+    df = df.reset_index(drop=True)
+
     # Assign buffer transfers to buffer plate
-    df.loc[ df["src_type"] == "buffer", "source_fc" ] = "buffer_plate"
+    df.loc[df["src_type"] == "buffer", "source_fc"] = "buffer_plate"
 
     # Assign buffer source wells
     if buffer_strategy == "column":
         # Keep rows, but only use column 1
-        df.loc[df["src_type"] == "buffer", "source_well"] = \
-            df.loc[df["src_type"] == "buffer", "source_well"].apply(lambda x : x[0:-1]+"1")
+        df.loc[df["src_type"] == "buffer", "source_well"] = df.loc[
+            df["src_type"] == "buffer", "source_well"
+        ].apply(lambda x: x[0:-1] + "1")
     else:
         raise Exception("No buffer strategy defined")
 
     return df
 
 
-
 def well2rowcol(well_iter):
-    """Translates iterable of well names to list of row/column integer tuples to specify well location in Mosquito worklists."""
-   
+    """
+    Translates iterable of well names to list of row/column integer tuples to specify
+    well location in Mosquito worklists.
+    """
+
     # In an advanced worklist: startcol, endcol, row
     rows = []
     cols = []
     for well in well_iter:
         [row_letter, col_number] = str.split(well, sep=":")
         rowdict = {}
-        for l,n in zip("ABCDEFGH","12345678"):
+        for l, n in zip("ABCDEFGH", "12345678"):
             rowdict[l] = n
         rows.append(rowdict[row_letter])
         cols.append(col_number)
     return rows, cols
-
 
 
 def get_filenames(method_name, pid):
@@ -151,34 +174,40 @@ def get_filenames(method_name, pid):
     return wl_filename, log_filename
 
 
-
-def write_worklist(df, deck, wl_filename, comments, strategy = "default"):
+def write_worklist(df, deck, wl_filename, comments=None, strategy="default"):
 
     # Format comments for printing into worklist
-    comments = ["COMMENT, " + e for e in comments]
+    if comments:
+        comments = ["COMMENT, " + e for e in comments]
 
     # Default transfer type is simple copy
     df["transfer_type"] = "COPY"
 
     if strategy == "multi-aspirate":
-        filter = np.all([
-            df.dst_pos == df.shift(-1).dst_pos,                  # End position of next transfer is the same
-            df.dest_well == df.shift(-1).dest_well,              # End well of the next transfer is the same
-            df.source_fc == "buffer_plate",                      # This transfer is buffer
-            df.shift(-1).source_fc != "buffer_plate",            # Next transfer is not buffer
-            df.transfer_vol + df.shift(-1).transfer_vol <= 5000  # Sum of this and next transfer is >= 5 ul
-            ], axis=0)
+        filter = np.all(
+            [
+                df.dst_pos
+                == df.shift(-1).dst_pos,  # End position of next transfer is the same
+                df.dest_well
+                == df.shift(-1).dest_well,  # End well of the next transfer is the same
+                df.source_fc == "buffer_plate",  # This transfer is buffer
+                df.shift(-1).source_fc != "buffer_plate",  # Next transfer is not buffer
+                df.transfer_vol + df.shift(-1).transfer_vol
+                <= 5000,  # Sum of this and next transfer is >= 5 ul
+            ],
+            axis=0,
+        )
         df.loc[filter, "transfer_type"] = "MULTI_ASPIRATE"
-        
+
     # PRECAUTION Keep tip change strategy in a single dict to avoid mix-ups
     tip_strats = {
-        "always" : ("[VAR1]", "TipChangeStrategy,always"),
-        "never" : ("[VAR2]", "TipChangeStrategy,never")
+        "always": ("[VAR1]", "TipChangeStrategy,always"),
+        "never": ("[VAR2]", "TipChangeStrategy,never"),
     }
 
     # Convert all data to strings
     for c in df:
-        df.loc[:,c] = df[c].apply(str)
+        df.loc[:, c] = df[c].apply(str)
 
     # Write worklist
     with open(wl_filename, "w") as wl:
@@ -194,27 +223,37 @@ def write_worklist(df, deck, wl_filename, comments, strategy = "default"):
         # Write transfers
         for i, r in df.iterrows():
             if r.transfer_type == "COPY":
-                wl.write(",".join([
-                    r.transfer_type, 
-                    r.src_pos, 
-                    r.src_col, 
-                    r.src_col, 
-                    r.src_row, 
-                    r.dst_pos, 
-                    r.dst_col, 
-                    r.dst_row,
-                    r.transfer_vol, 
-                    tip_strats["always"][0]
-                    ]) + "\n")
+                wl.write(
+                    ",".join(
+                        [
+                            r.transfer_type,
+                            r.src_pos,
+                            r.src_col,
+                            r.src_col,
+                            r.src_row,
+                            r.dst_pos,
+                            r.dst_col,
+                            r.dst_row,
+                            r.transfer_vol,
+                            tip_strats["always"][0],
+                        ]
+                    )
+                    + "\n"
+                )
             elif r.transfer_type == "MULTI_ASPIRATE":
-                wl.write(",".join([
-                    r.transfer_type, 
-                    r.src_pos, 
-                    r.src_col,  
-                    r.src_row, 
-                    "1",
-                    r.transfer_vol, 
-                    ]) + "\n")
+                wl.write(
+                    ",".join(
+                        [
+                            r.transfer_type,
+                            r.src_pos,
+                            r.src_col,
+                            r.src_row,
+                            "1",
+                            r.transfer_vol,
+                        ]
+                    )
+                    + "\n"
+                )
             else:
                 raise AssertionError("No transfer type defined")
 
@@ -223,7 +262,7 @@ def get_deck_comment(deck):
 
     pos2plate = dict([(pos, plate) for plate, pos in deck.items()])
 
-    l = [pos2plate[i] if i in pos2plate else "[Empty]" for i in range(1,6)]
+    l = [pos2plate[i] if i in pos2plate else "[Empty]" for i in range(1, 6)]
 
     deck_comment = "COMMENT, Set up layout:    " + "     ".join(l) + "\n"
 
@@ -233,7 +272,7 @@ def get_deck_comment(deck):
 def upload_log(currentStep, lims, log, log_filename):
     with open(log_filename, "w") as logContext:
         logContext.write("\n".join(log))
-    
+
     for out in currentStep.all_outputs():
         if out.name == "Mosquito Log":
             for f in out.files:
--- scripts/zika_methods.py
+++ scripts/zika_methods.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+DESC = """
+This module contains functions used to generate worklists for the Mosquito X1
+instrument Zika using sample data fetched from Illumina Clarity LIMS.
+
+Each function corresponds to a single method / application and is tied to a
+specific workflow step.
+
+Written by Alfred Kedhammar
+"""
+
 import zika
 import pandas as pd
 import sys
@@ -20,10 +30,12 @@ def setup_QIAseq(currentStep, lims):
         "dest_well",
         "dest_fc_name",
         "target_vol",
-        "target_amt"
+        "target_amt",
     ]
     df = zika.fetch_sample_data(currentStep, to_fetch)
-    assert all(df.conc_units == "ng/ul"), "All sample concentrations are expected in 'ng/ul'"
+    assert all(
+        df.conc_units == "ng/ul"
+    ), "All sample concentrations are expected in 'ng/ul'"
 
     # Define constraints
     min_zika_vol = 0.1
@@ -38,48 +50,53 @@ def setup_QIAseq(currentStep, lims):
     assert len(df.source_fc.unique()) == 1, "Only one input plate allowed"
     assert len(df.dest_fc.unique()) == 1, "Only one output plate allowed"
     deck = {
-        "buffer_plate" : 2,
-        df.source_fc.unique()[0] : 3,
-        df.dest_fc.unique()[0] : 4,
-        }
+        "buffer_plate": 2,
+        df.source_fc.unique()[0]: 3,
+        df.dest_fc.unique()[0]: 4,
+    }
 
     # Cases 1) - 3)
-    d = {"sample" : [], "buffer" : [], "tot_vol" : []}
+    d = {"sample": [], "buffer": [], "tot_vol": []}
     log = []
     for i, r in df.iterrows():
 
         # 1) Sample too dilute
         if r.max_transfer_amt < r.target_amt:
-            
+
             sample_vol = min(r.target_vol, r.vol)
             tot_vol = r.target_vol
             buffer_vol = tot_vol - sample_vol
 
-            target_pc = round((sample_vol * r.conc / tot_vol) / r.target_conc * 100,2)
-            log.append(f"WARNING: Insufficient amount of sample {r.sample_name} (conc {r.conc} ng/ul, vol {r.vol} ul)")
+            target_pc = round((sample_vol * r.conc / tot_vol) / r.target_conc * 100, 2)
+            log.append(
+                f"WARNING: Insufficient amount of sample {r.sample_name} (conc {r.conc} ng/ul, vol {r.vol} ul)"
+            )
             log.append(f"\t--> Reaching {target_pc}% of target concentration")
 
             # TODO change udf accordingly
 
         # 2) Ideal case
         elif r.min_transfer_amt <= r.target_amt <= r.max_transfer_amt:
-            
+
             sample_vol = r.target_amt / r.conc
             buffer_vol = r.target_vol - sample_vol
             tot_vol = sample_vol + buffer_vol
-        
+
         # 3) Sample too concentrated -> Increase final volume if possible
         elif r.min_transfer_amt > r.target_amt:
-            
+
             increased_vol = r.min_transfer_amt / r.target_conc
-            assert increased_vol < max_final_vol, \
-                f"Sample {r.name} is too concentrated ({r.conc} ng/ul) and must be diluted manually"
+            assert (
+                increased_vol < max_final_vol
+            ), f"Sample {r.name} is too concentrated ({r.conc} ng/ul) and must be diluted manually"
 
             tot_vol = increased_vol
             sample_vol = min_zika_vol
             buffer_vol = tot_vol - sample_vol
 
-            log.append(f"WARNING: High concentration of sample {r.sample_name} (conc {r.conc} ng/ul, vol {r.vol} ul)")
+            log.append(
+                f"WARNING: High concentration of sample {r.sample_name} (conc {r.conc} ng/ul, vol {r.vol} ul)"
+            )
             log.append(f"\t--> Adjusting total volume to {tot_vol} ul")
 
             # TODO change udf accordingly
@@ -87,35 +104,34 @@ def setup_QIAseq(currentStep, lims):
         d["sample"].append(sample_vol)
         d["buffer"].append(buffer_vol)
         d["tot_vol"].append(tot_vol)
-    
+
     df = df.join(pd.DataFrame(d))
 
     # Generate Mosquito-readable columns
-    df = zika.format_worklist(df, deck = deck, buffer_strategy = "column")
+    df = zika.format_worklist(df, deck=deck, buffer_strategy="column")
 
     # Comments to attach to the worklist header
     n_samples = len(df[df.src_type == "sample"])
-    comments = [
-        f"This worklist will enact normalization of {n_samples} samples"
-    ]
+    comments = [f"This worklist will enact normalization of {n_samples} samples"]
 
     # Write files and upload
     method_name = "setup_QIAseq"
     wl_filename, log_filename = zika.get_filenames(method_name, currentStep.id)
 
     zika.write_worklist(
-        df= df,
-        deck = deck,
-        wl_filename = wl_filename,
-        comments = comments,
-        strategy = "multi-aspirate"
-        )
+        df=df,
+        deck=deck,
+        wl_filename=wl_filename,
+        comments=comments,
+        strategy="multi-aspirate",
+    )
 
     zika.upload_log(currentStep, lims, log, log_filename)
     zika.upload_csv(currentStep, lims, wl_filename)
-    
+
     # Issue warnings, if any
     if any("WARNING:" in entry for entry in log):
-        sys.stderr.write("CSV-file generated with warnings, please check the Log file\n")
+        sys.stderr.write(
+            "CSV-file generated with warnings, please check the Log file\n"
+        )
         sys.exit(2)
-
