e72d24af2f5210bf972bec4b72692904a4a03bc6
kedhammar <alfred.kedhammar@scilifelab.se>
2022-11-21 10:14:30 +0100

    Simplify comment

--- scripts/bravo_csv.py
+++ scripts/bravo_csv.py
@@ -796,7 +796,7 @@ def zika_norm(lims, currentStep):
 
 def default_bravo(lims, currentStep, with_total_vol=True):
 
-    # Zika for Pico RNA Library prep
+    # Re-route to Zika
     if zika.verify_step(
         currentStep, 
         targets = [
