833bae7746e72d4b480ad3350d76dd2ac2081ecb
kedhammar <alfred.kedhammar@scilifelab.se>
2023-01-18 11:06:14 +0100

    Start accessing the input samples

--- scripts/ont_samplesheet.py
+++ scripts/ont_samplesheet.py
@@ -9,7 +9,31 @@ from genologics.lims import Lims
 from genologics.config import BASEURI, USERNAME, PASSWORD
 from scilifelab_epps.epp import attach_file
 from genologics.entities import Process
+from datetime import datetime as dt
+
+DESC = """EPP used to generate a MinKNOW sample sheet for ONT samples"""
 
 def main(lims, args):
+
     currentStep = Process(lims, id=args.pid)
 
+    file_meta = {"pid":currentStep.id, "timestamp":dt.now()}
+    log = []
+
+    samples = [art_tuple[0]['uri'] for art_tuple in currentStep.input_output_maps \
+        if art_tuple[0]['uri'].type == "Analyte"]
+
+    for sample in samples:
+        pass
+
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=DESC)
+    parser.add_argument('--pid',
+                        help='Lims id for current Process')
+    args = parser.parse_args()
+
+    lims = Lims(BASEURI, USERNAME, PASSWORD)
+    lims.check_version()
+    main(lims, args)
