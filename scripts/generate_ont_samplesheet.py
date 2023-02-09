21925c49b91aa509f54404cadd2118a8f59dae32
kedhammar <alfred.kedhammar@scilifelab.se>
2023-02-09 14:08:13 +0100

    WIP

--- scripts/generate_ont_samplesheet.py
+++ scripts/generate_ont_samplesheet.py
@@ -7,6 +7,7 @@ from genologics.config import BASEURI, USERNAME, PASSWORD
 from genologics.entities import Process
 from datetime import datetime as dt
 import pandas as pd
+import re
 
 DESC = """EPP used to generate a MinKNOW sample sheet for ONT samples"""
 
@@ -14,22 +15,26 @@ DESC = """EPP used to generate a MinKNOW sample sheet for ONT samples"""
 def main(lims, args):
     """ Barcoding vs no-barcoding, devices and experiments cannot be mixed in a samplesheet and must be split across multiple ones.
     Create many sample sheets and deliver them as a zipped file.
+
+    experiment_id   The LIMS step generating the samplesheet(s), concatenated with the specified sample sheet name, e.g. "12-34567_P12345-many-flowcells"
     """
 
     currentStep = Process(lims, id=args.pid)
 
     arts = [art for art in currentStep.all_inputs() \
         if art.type == "Analyte"]
+    
+    timestamp = dt.now().strftime("%y%m%d_%H%M%S")
 
     rows = []
     for art in arts:
 
         row = {
-            "sheet_name": art.udf.get('ONT sample sheet name'),
+            "sheet_name": strip_characters(art.udf.get('ONT sample sheet name')),
             "instrument": art.udf.get("ONT flow cell type"),
             "flow_cell_id": art.udf.get("ONT flow cell ID"),
-            "sample_id": art.name,
-            "experiment_id": f"{art.samples[0].project.id}_{art.udf.get('ONT sample sheet name')}",
+            "sample_id": strip_characters(get_minknow_sample_id(art)),
+            "experiment_id": f"{currentStep.id}_{timestamp}_{strip_characters(art.udf.get('ONT sample sheet name'))}",
             "flow_cell_product_code": get_fc_product_code(art),
             "kit": get_kit_string(art)
         }
@@ -43,8 +48,8 @@ def main(lims, args):
         # Pool
         else:
             for sample, label in zip(art.samples, art.reagent_labels):
-                row["alias"] = sample.name
-                row["barcode"] = "barcode" + label[0:2]
+                row["alias"] = strip_characters(sample.name)
+                row["barcode"] = strip_characters("barcode" + label[0:2])
                 rows.append(row.copy())
 
     df = pd.DataFrame(rows)
@@ -54,24 +59,77 @@ def main(lims, args):
 
     # Iterate across sheets
     sheets = df.sheet_name.unique()
-    common_file_suffix = f"{currentStep.id}_{dt.now().strftime('%y%m%d_%H%M%S')}"
     for sheet in sheets:
         
+        # Subset dataframe to current sheet
         df_sheet = df[df.sheet_name == sheet]
 
-        if 
-        df_csv = df_sheet.loc[:, "flow_cell_id" : "barcode"]
-        write_csv(df_sheet, common_file_suffix)
+        # Barcodes
+        if df_sheet[df_sheet.alias == ""].empty and df_sheet[df_sheet.barcode == ""].empty:
+
+            # Assert aliases and barcodes are unique
+            assert len(df_sheet.alias.unique()) == len(df_sheet)
+            assert len(df_sheet.barcode.unique()) == len(df_sheet)
+
+        # No barcodes
+        elif df_sheet[df_sheet.alias != ""].empty and df_sheet[df_sheet.barcode != ""].empty:
+            
+            # Trim away unused columns
+            df_sheet = df_sheet.loc[:, "sheet_name" : "kit"]
+
+        else:
+            # Sheet dataframe rows must either all have barcodes or none have barcodes
+            raise AssertionError
+
+        # Assert sheet contains only one experiment
+        assert len(df_sheet.experiment_id.unique()) == 1
+        # Assert sheet contains only one instrument
+        assert len(df_sheet.instrument.unique()) == 1
+
+        file_list = []
+        write_csv(df_sheet, file_list)
 
     upload_csv(df, currentStep)
 
 
-def write_csv(df_sheet):
+def write_csv(df_sheet, file_list):
+
+    file_name = f"ONT_samplesheet_{df_sheet.experiment_id.unique()[0]}.csv"
+    file_list.append(file_name)
+
+    df_csv = df_sheet.loc[:, [
+        "flow_cell_id",
+        "sample_id",
+        "experiment_id",
+        "flow_cell_product_code",
+        "kit"
+    ]]
+
+    df_csv.to_csv(file_name, index = False)
+
+def get_minknow_sample_id(art):
+
+    sample_id_pattern = re.compile("(P\d{5})_(\d+)")
+
+    if re.match(sample_id_pattern, art.name):
+        return re.match(sample_id_pattern, art.name).group()
     
+    elif re.match(sample_id_pattern, art.samples[0].name):
+        return f"{re.match(sample_id_pattern, art.samples[0].name).groups()[0]}_{art.name}"
+
+    else:
+        return None
 
 
-def zip_csvs(filenames):
-    pass
+def strip_characters(input_string):
+
+    allowed_characters = re.compile("[^a-zA-Z0-9_-]")
+    subbed_string = allowed_characters.sub("_", input_string)
+
+    string_to_shorten = re.compile("__+")
+    shortened_string = string_to_shorten.sub("_", subbed_string)
+
+    return shortened_string
 
 
 def upload_csv(df, currentStep, lims):
