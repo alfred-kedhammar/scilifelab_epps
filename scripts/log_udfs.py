fc051072cc9a288bd1732cf19aa40818bb51235f
kedhammar <alfred.kedhammar@scilifelab.se>
2023-02-27 10:36:18 +0100

    Rename script and add input assertions to log_udfs

--- scripts/log_udfs.py
+++ scripts/log_udfs.py
@@ -6,68 +6,85 @@ from genologics.lims import Lims
 from genologics.config import BASEURI, USERNAME, PASSWORD
 from genologics.entities import Process
 from datetime import datetime as dt
-import pandas as pd
 from tabulate import tabulate
+from send_ont_flowcell_info_to_statusdb import assert_input
+import pandas as pd
+import sys
 
-DESC = """ Script for the EPP "Log fields" and file slot "Field log".
-Use this script to create / update an append-only log where the step UDF info is stored """
+
+DESC = """
+Script for the EPP "Log fields" and file slot "Field log".
+Use this script to create / update an append-only log where the step UDF info is stored.
+Step-specific input assertions may be added where indicated.
+"""
 
 
 def main(lims, args):
 
-    currentStep = Process(lims, id=args.pid)
+    try:
 
-    timestamp = dt.now().strftime("%y%m%d_%H%M%S")
+        currentStep = Process(lims, id=args.pid)
 
-    # Collect last log file, if there is one
-    file_art = [op for op in currentStep.all_outputs() if op.name == "Field log"][0]
-    if file_art.files:
-        file_bytes = lims.get_file_contents(uri = file_art.files[0].uri).read()
-        file_str = file_bytes.decode("utf-8")
-    else:
-        file_str = None
-                
-    # Parse outputs and their UDFs
-    outputs = [output for output in currentStep.all_outputs() if output.type == "Analyte"]
+        timestamp = dt.now().strftime("%y%m%d_%H%M%S")
 
-    # Collect all populated UDFs
-    output_udfs = []
-    for output in outputs:
-        for udf_tuple in output.udf.items():
-            if udf_tuple[0] not in output_udfs:
-                output_udfs.append(udf_tuple[0])
+        # Collect last log file, if there is one
+        file_art = [op for op in currentStep.all_outputs() if op.name == "Field log"][0]
+        if file_art.files:
+            file_bytes = lims.get_file_contents(uri = file_art.files[0].uri).read()
+            file_str = file_bytes.decode("utf-8")
+        else:
+            file_str = None
+                    
+        # Parse outputs and their UDFs
+        outputs = [output for output in currentStep.all_outputs() if output.type == "Analyte"]
 
-    # Start building log dataframe
-    rows = []
-    for output in outputs:
-        row = {}
-        row["Sample"] = output.name
-        for udf in output_udfs:
-            try:
-                row[udf] = output.udf[udf]
-            except KeyError:
-                row[udf] = None
-        rows.append(row)
+        # Collect all populated UDFs
+        output_udfs = []
+        for output in outputs:
+            for udf_tuple in output.udf.items():
+                if udf_tuple[0] not in output_udfs:
+                    output_udfs.append(udf_tuple[0])
 
-    df = pd.DataFrame(rows).set_index("Sample")
+        # Start building log dataframe
+        rows = []
+        for output in outputs:
+            row = {}
+            row["Sample"] = output.name
+            for udf in output_udfs:
+                try:
+                    row[udf] = output.udf[udf]
+                except KeyError:
+                    row[udf] = None
 
-    new_log_name = f"UDF_log_{currentStep.id}_{timestamp}.txt"
-    with open(new_log_name, "w") as f:
-        
-        # Write previous file contents
-        if file_str:
-            f.write(file_str)
+            # Step-specific assertions
+            if 'ONT Sequencing and Reloading' in currentStep.type.name:
+                assert_input(row)
 
-        # Write new file contents
-        f.write(timestamp+"\n")
-        f.write(tabulate(df, headers="keys"))
-        f.write("\n\n")
+            rows.append(row)
 
-    for out in currentStep.all_outputs():
-        if out.name == "Field log":
-            for f in out.files:
-                lims.request_session.delete(f.uri)
-            lims.upload_new_file(out, new_log_name)
+        df = pd.DataFrame(rows).set_index("Sample")
+
+        new_log_name = f"UDF_log_{currentStep.id}_{timestamp}.txt"
+        with open(new_log_name, "w") as f:
+            
+            # Write previous file contents
+            if file_str:
+                f.write(file_str)
+
+            # Write new file contents
+            f.write(timestamp+"\n")
+            f.write(tabulate(df, headers="keys"))
+            f.write("\n\n")
+
+        for out in currentStep.all_outputs():
+            if out.name == "Field log":
+                for f in out.files:
+                    lims.request_session.delete(f.uri)
+                lims.upload_new_file(out, new_log_name)
+
+    except AssertionError as e:
+        sys.stderr.write(str(e))
+        sys.exit(2)
 
 
 if __name__ == "__main__":
--- scripts/send_ont_flowcell_info_to_statusdb.py
+++ scripts/send_ont_flowcell_info_to_statusdb.py
@@ -12,9 +12,9 @@ import couchdb
 import yaml
 import sys
 from generate_ont_samplesheet import get_minknow_sample_id
-import log_udfs
 
-DESC = """ Script for EPP "Send ONT reloading info to StatusDB".
+
+DESC = """ Script for EPP "Send ONT flowcell info to StatusDB".
 Used to record the washing and reloading of ONT flow cells.
 Information is parsed from LIMS and uploaded to the CouchDB database nanopore_runs.
 """
@@ -54,19 +54,7 @@ def main(lims, args):
             fc["reload_lots"] =  art_tuple[1]["uri"].udf.get("ONT reload wash kit").replace(" ","").split(",") if \
                                 art_tuple[1]["uri"].udf.get("ONT reload wash kit") else None
 
-            # Assert correct input
-            assert check_csv_udf_list("^\d{2,3}:\d{2}$", fc["reload_times"]), \
-                "Reload run times must be formatted as comma-separated hh:mm"
-            check_times_list(fc["reload_times"])
-            assert check_csv_udf_list("^[0-9.]+$", fc["reload_fmols"]), \
-                "Invalid flow cell reload amount(s)"
-            assert check_csv_udf_list("^[0-9a-zA-Z.-_]+$", fc["reload_lots"]), \
-                "Invalid Reload wash kit"
-            
-            if fc["reload_times"] or fc["reload_fmols"] or fc["reload_lots"]:
-                assert fc["reload_times"] and fc["reload_fmols"] and fc["reload_lots"] and \
-                    len(fc["reload_times"]) == len(fc["reload_fmols"]) == len(fc["reload_lots"]), \
-                    "All reload UDFs within a row must have the same number of comma-separated values"
+            assert_input(fc)
 
             fcs.append(fc)
 
@@ -124,12 +112,27 @@ def main(lims, args):
                                 "If the run was recently started, wait until it appears in GenStat.")
                 sys.exit(2)
 
-        # Also update log after uploading to StatusDB
-        log_udfs.main(lims, args)
 
     except AssertionError as e:
         sys.stderr.write(str(e))
         sys.exit(2)
+
+
+def assert_input(fc):
+
+    # Assert correct input
+    assert check_csv_udf_list("^\d{2,3}:\d{2}$", fc["reload_times"]), \
+        "Reload run times must be formatted as comma-separated hh:mm"
+    check_times_list(fc["reload_times"])
+    assert check_csv_udf_list("^[0-9.]+$", fc["reload_fmols"]), \
+        "Invalid flow cell reload amount(s)"
+    assert check_csv_udf_list("^[0-9a-zA-Z.-_]+$", fc["reload_lots"]), \
+        "Invalid Reload wash kit"
+    
+    if fc["reload_times"] or fc["reload_fmols"] or fc["reload_lots"]:
+        assert fc["reload_times"] and fc["reload_fmols"] and fc["reload_lots"] and \
+            len(fc["reload_times"]) == len(fc["reload_fmols"]) == len(fc["reload_lots"]), \
+            "All reload UDFs within a row must have the same number of comma-separated values"
 
 
 def check_times_list(times_list):
