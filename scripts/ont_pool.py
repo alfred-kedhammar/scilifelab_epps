9c25e4754cb08dcc7bc090d3e8fedc8f228f816c
kedhammar <alfred.kedhammar@scilifelab.se>
2023-03-17 07:58:43 +0100

    WIP pooling code

--- scripts/ont_pool.py
+++ scripts/ont_pool.py
@@ -3,8 +3,9 @@ from argparse import ArgumentParser
 from genologics.lims import Lims
 from genologics.config import BASEURI, USERNAME, PASSWORD
 from genologics.entities import Process
-from ont_update_amount import fetch_last
 from zika_utils import fetch_sample_data
+from molar_concentration import calculate_fmol
+from numpy import minimum
 
 DESC = """
 EPP "ONT pooling"
@@ -12,45 +13,48 @@ Given a target fmol amount and total volume, try to create an equimolar pool.
 """
 
 def main(lims, args):
-        
+        """
+        1) Find molar proportions of samples within each pool
+        2) Apply inverse molar proprotions to target pool volume to get the sample
+           volumes corresponding to equimolar pooling.
+        3) 
+        """
+
         currentStep = Process(lims, id=args.pid)
 
         pools = [art for art in currentStep.all_outputs() if art.type == "Analyte"]
         pools.sort(key=lambda pool: pool.name)
         
         to_fetch = {
-            # Input sample
-            "sample_name"       :       "art_tuple[0]['uri'].name",
-            "vol"               :       "art_tuple[0]['uri'].udf['Volume (ul)']",
-            "conc"              :       "art_tuple[0]['uri'].udf['Concentration']",
-            "dst_id"            :       "art_tuple[1]['uri'].location[0].id",
-            "dst_well"          :       "art_tuple[1]['uri'].location[1]",
-            "target_amt"        :       "art_tuple[1]['uri'].udf['Amount (fmol)']",
-            "target_vol"        :       "art_tuple[1]['uri'].udf['Final Volume (uL)']",
+            # Search within step
+            "sample_name"          : "art_tuple[0]['uri'].name",
+            "vol"                  : "art_tuple[0]['uri'].udf['Final Volume (uL)']",
+            "conc_ng"              : "art_tuple[0]['uri'].udf['Final Concentration']",
+            "dst_name"             : "art_tuple[1]['uri'].name",
+            "dst_id"               : "art_tuple[1]['uri'].location[0].id",
+            "pool_target_amt_fmol" : "art_tuple[1]['uri'].udf['Amount (fmol)']",
+            "pool_target_vol"      : "art_tuple[1]['uri'].udf['Final Volume (uL)']",
+            # Seach recursively
+            "size"                 : "Size (bp)",
         }
             
-        fetch_sample_data(currentStep, to_fetch)
+        df = fetch_sample_data(currentStep, to_fetch)
 
-        d = {}
-        for art_tuple in art_tuples:
-            
-            # Fetch info
-            target_amt = art_tuple[1]["uri"].udf["Amount (fmol)"] / len(art_tuples)
-            target_vol = art_tuple[1]["uri"].udf["Total Volume (uL)"]
+        df["conc_nM"] = df.apply(lambda x: calculate_fmol(x["conc_ng"], x["size"]), axis = 1)
+        df["amt_ng"] = df.conc_ng * df.vol
+        df["amt_fmol"] = df.conc_nM * df.vol
 
-            prev_amt = art_tuple[0]["uri"].udf["Amount (fmol)"]
-            if fetch_last(currentStep, art_tuple, "Final Volume (uL)"):
-                prev_vol = fetch_last(currentStep, art_tuple, "Final Volume (uL)")
-            else:
-                 prev_vol = fetch_last(currentStep, art_tuple, "Volume (ul)")
+        for pool in pools:
+            # Subset data
+            df_pool = df[df.dst_name == pool.name].copy()
 
-            # This is calculated
-            vol_to_take = min((target_amt / prev_amt) * prev_vol, prev_vol, target_vol)
+            target_amt_fmol = df_pool.pool_target_amt_fmol[0] / len(df_pool)
+            pool_target_amt_fmol = df_pool.pool_target_amt_fmol[0]
 
-            # Put
-            art_tuple[1]["uri"].udf["Amount (fmol)"] = min(prev_amt, target_amt)
-            art_tuple[1]["uri"].udf["Volume to take (uL)"] = vol_to_take
-            art_tuple[1]["uri"].put()
+            df_pool["prop_nM"] = df_pool.conc_nM / sum(df_pool.conc_nM)
+            df_pool["prop_nM_inv"] = (1 / df_pool["prop_nM"]) / sum((1 / df_pool["prop_nM"]))
+
+            df_pool["sample_vol"] = minimum(df_pool.pool_target_amt_fmol[0] / df_pool.conc_nM, df_pool.vol)
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=DESC)
