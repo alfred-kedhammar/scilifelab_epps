65d068785c3bc93cd4f54f84be0f5e4268042a38
kedhammar <alfred.kedhammar@scilifelab.se>
2023-03-17 14:02:03 +0100

    Functional, happy :)

--- scripts/ont_pool.py
+++ scripts/ont_pool.py
@@ -6,6 +6,10 @@ from genologics.entities import Process
 from zika_utils import fetch_sample_data
 from molar_concentration import calculate_fmol
 from numpy import minimum
+from tabulate import tabulate
+from pandas.api.types import is_numeric_dtype
+from datetime import datetime as dt
+import pandas as pd
 
 DESC = """
 EPP "ONT pooling"
@@ -21,6 +25,7 @@ def main(lims, args):
         """
 
         currentStep = Process(lims, id=args.pid)
+        log = []
 
         pools = [art for art in currentStep.all_outputs() if art.type == "Analyte"]
         pools.sort(key=lambda pool: pool.name)
@@ -28,33 +33,86 @@ def main(lims, args):
         to_fetch = {
             # Search within step
             "sample_name"          : "art_tuple[0]['uri'].name",
-            "vol"                  : "art_tuple[0]['uri'].udf['Final Volume (uL)']",
-            "conc_ng"              : "art_tuple[0]['uri'].udf['Final Concentration']",
             "dst_name"             : "art_tuple[1]['uri'].name",
-            "dst_id"               : "art_tuple[1]['uri'].location[0].id",
-            "pool_target_amt_fmol" : "art_tuple[1]['uri'].udf['Amount (fmol)']",
-            "pool_target_vol"      : "art_tuple[1]['uri'].udf['Final Volume (uL)']",
+            "vol_ul"               : "art_tuple[0]['uri'].udf['Final Volume (uL)']",
+            "conc_ng_ul"           : "art_tuple[0]['uri'].udf['Final Concentration']",
             # Seach recursively
-            "size"                 : "Size (bp)",
+            "size_bp"              : "Size (bp)",
         }
             
         df = fetch_sample_data(currentStep, to_fetch)
 
-        df["conc_nM"] = df.apply(lambda x: calculate_fmol(x["conc_ng"], x["size"]), axis = 1)
-        df["amt_ng"] = df.conc_ng * df.vol
-        df["amt_fmol"] = df.conc_nM * df.vol
+        df["conc_nM"] = df.apply(lambda x: calculate_fmol(x["conc_ng_ul"], x["size_bp"]), axis = 1)
 
         for pool in pools:
+
+            log.append(f"{pool.name}")
+
             # Subset data
             df_pool = df[df.dst_name == pool.name].copy()
 
-            target_amt_fmol = df_pool.pool_target_amt_fmol[0] / len(df_pool)
-            pool_target_amt_fmol = df_pool.pool_target_amt_fmol[0]
-
+            # Get molar proportions between samples within pool
             df_pool["prop_nM"] = df_pool.conc_nM / sum(df_pool.conc_nM)
             df_pool["prop_nM_inv"] = (1 / df_pool["prop_nM"]) / sum((1 / df_pool["prop_nM"]))
 
-            df_pool["sample_vol"] = minimum(df_pool.pool_target_amt_fmol[0] / df_pool.conc_nM, df_pool.vol)
+            # If amount is specified, use for calculations and ignore target vol
+            try:
+                pool_target_amt_fmol = pool.udf["Amount (fmol)"]
+                pool_target_vol = None
+
+                log.append(f"Target amt: {round(pool_target_amt_fmol,1)} fmol")
+
+                target_amt_fmol = pool_target_amt_fmol / len(df_pool)
+
+                # Apply molar proportions to target amount to get transfer volumes
+                df_pool["transfer_vol_ul"] = minimum(target_amt_fmol / df_pool.conc_nM, df_pool.vol_ul)
+                pool_transfer_vol = sum(df_pool.transfer_vol_ul)
+                df_pool["transfer_amt_fmol"] = df_pool.transfer_vol_ul * df_pool.conc_nM
+                pool_transfer_amt = sum(df_pool.transfer_amt_fmol)
+
+            # If amount is omitted but not target volume, use target volume for calculations
+            except KeyError:
+                pool_target_amt_fmol = None
+                pool_target_vol = pool.udf['Final Volume (uL)']
+
+                log.append(f"Target vol: {round(pool_target_vol,1)} uL") 
+
+                # Apply molar proportions to target volume to get transfer amounts
+                df_pool["transfer_vol_ul"] = minimum(pool_target_vol * df_pool.prop_nM_inv, df_pool.vol_ul)
+                pool_transfer_vol = sum(df_pool.transfer_vol_ul)
+                df_pool["transfer_amt_fmol"] = df_pool.transfer_vol_ul * df_pool.conc_nM
+                pool_transfer_amt = sum(df_pool.transfer_amt_fmol)                
+
+            # Evaluate target fraction
+            df_pool["molar_percentage"] = df_pool.transfer_amt_fmol / sum(df_pool.transfer_amt_fmol) * 100
+            
+            # Update UDFs
+            pool.udf["Amount (fmol)"] = round(pool_transfer_amt,1)
+            pool.udf['Final Volume (uL)'] = round(pool_transfer_vol, 1)
+            pool.put()
+
+            # Create table
+            df_to_print = df_pool.loc[:,["sample_name", "vol_ul", "conc_ng_ul", "size_bp", "conc_nM", "transfer_vol_ul", "transfer_amt_fmol", "molar_percentage"]]
+            df_to_print.reset_index(inplace = True, drop = True)
+            df_to_print = df_to_print.round(1)
+            log.append(tabulate(df_to_print, headers="keys"))
+            log.append(f"\nFinal amt: {round(pool_transfer_amt, 1)} fmol")
+            log.append(f"Final vol: {round(pool_transfer_vol,1)} uL")
+            log.append("\n")
+
+        # Write log
+        timestamp = dt.now().strftime("%y%m%d_%H%M%S")
+        log_filename =  "_".join(["ont_pooling_log", currentStep.id, timestamp]) + ".log"
+        with open(log_filename, "w") as logContext:
+            logContext.write("\n".join(log))
+
+        # Upload log
+        for out in currentStep.all_outputs():
+            if out.name == "ONT pooling log":
+                for f in out.files:
+                    lims.request_session.delete(f.uri)
+                lims.upload_new_file(out, log_filename)
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=DESC)
