4e2aeea72f8f9ab7ad16a85eb3f484eafd87cec5
kedhammar <alfred.kedhammar@scilifelab.se>
2023-04-17 10:50:02 +0200

    Fix excessive indent

--- scripts/ont_calc_volumes.py
+++ scripts/ont_calc_volumes.py
@@ -24,59 +24,59 @@ Will use ONE of the output UDFs (prioritized in the listed order) to calculate a
 
 def main(lims, args):
         
-        currentStep = Process(lims, id=args.pid)
-        art_tuples = [art_tuple for art_tuple in currentStep.input_output_maps if art_tuple[1]["output-type"] == "Analyte"]
+    currentStep = Process(lims, id=args.pid)
+    art_tuples = [art_tuple for art_tuple in currentStep.input_output_maps if art_tuple[1]["output-type"] == "Analyte"]
 
-        for art_tuple in art_tuples:
-            
-            size = fetch_last(currentStep, art_tuple, "Size (bp)")
-            conc_units = art_tuple[0]["uri"].udf['Conc. Units']
+    for art_tuple in art_tuples:
+        
+        size = fetch_last(currentStep, art_tuple, "Size (bp)")
+        conc_units = art_tuple[0]["uri"].udf['Conc. Units']
 
-            # Fetch target amount, either fmol or ng
+        # Fetch target amount, either fmol or ng
+        try:
+            target_amt_fmol = art_tuple[1]["uri"].udf["Amount (fmol)"]
+            target_amt_ng = fmol_to_ng(target_amt_fmol, size)
+            basis = "fmol"
+        except KeyError:
             try:
-                target_amt_fmol = art_tuple[1]["uri"].udf["Amount (fmol)"]
-                target_amt_ng = fmol_to_ng(target_amt_fmol, size)
-                basis = "fmol"
+                target_amt_ng = art_tuple[1]["uri"].udf["Amount (ng)"]
+                target_amt_fmol = ng_to_fmol(target_amt_ng, size)
+                basis = "ng"
             except KeyError:
-                try:
-                    target_amt_ng = art_tuple[1]["uri"].udf["Amount (ng)"]
-                    target_amt_fmol = ng_to_fmol(target_amt_ng, size)
-                    basis = "ng"
-                except KeyError:
-                    target_vol = art_tuple[1]["uri"].udf["Volume to take (uL)"]
-                    basis = "vol"
+                target_vol = art_tuple[1]["uri"].udf["Volume to take (uL)"]
+                basis = "vol"
 
-            # Fetch last known sample volume
-            if fetch_last(currentStep, art_tuple, "Final Volume (uL)"):
-                prev_vol = fetch_last(currentStep, art_tuple, "Final Volume (uL)")
-            else:
-                prev_vol = fetch_last(currentStep, art_tuple, "Volume (ul)")
+        # Fetch last known sample volume
+        if fetch_last(currentStep, art_tuple, "Final Volume (uL)"):
+            prev_vol = fetch_last(currentStep, art_tuple, "Final Volume (uL)")
+        else:
+            prev_vol = fetch_last(currentStep, art_tuple, "Volume (ul)")
 
-            # Calculate
-            if basis == "fmol" or basis == "ng":
-                if conc_units == "nM":
-                    target_vol = target_amt_fmol / art_tuple[0]["uri"].udf["Concentration"]
-                elif conc_units == "ng/ul":
-                    target_vol = target_amt_ng / art_tuple[0]["uri"].udf["Concentration"]
-            
-            vol_to_take = min(
-                target_vol,
-                prev_vol
-            )
-
+        # Calculate
+        if basis == "fmol" or basis == "ng":
             if conc_units == "nM":
-                amt_taken_fmol = vol_to_take * art_tuple[0]["uri"].udf["Concentration"]
-                amt_taken_ng = fmol_to_ng(amt_taken_fmol, size)
-
+                target_vol = target_amt_fmol / art_tuple[0]["uri"].udf["Concentration"]
             elif conc_units == "ng/ul":
-                amt_taken_ng = vol_to_take * art_tuple[0]["uri"].udf["Concentration"]
-                amt_taken_fmol = ng_to_fmol(amt_taken_ng, size)
+                target_vol = target_amt_ng / art_tuple[0]["uri"].udf["Concentration"]
+        
+        vol_to_take = min(
+            target_vol,
+            prev_vol
+        )
 
-            if "Amount (ng)" in [t[0] for t in art_tuple[1]["uri"].udf.items()]:
-                art_tuple[1]["uri"].udf["Amount (ng)"] = round(amt_taken_ng,1)
-            art_tuple[1]["uri"].udf["Amount (fmol)"] = round(amt_taken_fmol,1)
-            art_tuple[1]["uri"].udf["Volume to take (uL)"] = round(vol_to_take,1)
-            art_tuple[1]["uri"].put()
+        if conc_units == "nM":
+            amt_taken_fmol = vol_to_take * art_tuple[0]["uri"].udf["Concentration"]
+            amt_taken_ng = fmol_to_ng(amt_taken_fmol, size)
+
+        elif conc_units == "ng/ul":
+            amt_taken_ng = vol_to_take * art_tuple[0]["uri"].udf["Concentration"]
+            amt_taken_fmol = ng_to_fmol(amt_taken_ng, size)
+
+        if "Amount (ng)" in [t[0] for t in art_tuple[1]["uri"].udf.items()]:
+            art_tuple[1]["uri"].udf["Amount (ng)"] = round(amt_taken_ng,1)
+        art_tuple[1]["uri"].udf["Amount (fmol)"] = round(amt_taken_fmol,1)
+        art_tuple[1]["uri"].udf["Volume to take (uL)"] = round(vol_to_take,1)
+        art_tuple[1]["uri"].put()
 
 
 if __name__ == "__main__":
