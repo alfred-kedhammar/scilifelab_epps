5d05da87b27c9230b6900a9fcf9bc2b63000196a
kedhammar <alfred.kedhammar@scilifelab.se>
2023-12-19 14:59:07 +0100

    warnings

--- scripts/parse_ba_results.py
+++ scripts/parse_ba_results.py
@@ -83,15 +83,17 @@ def main(lims, args):
                 f"Found .xml sample '{xml_sample_name}' matching {count_per}-wise well number {lims_well_num}."
             )
 
-            if re.search(lims_art.name, ngi_sample_id_pattern):
+            try:
                 lims_sample_name = re.search(
-                    lims_art.name, ngi_sample_id_pattern
+                    ngi_sample_id_pattern, lims_art.name
                 ).group()
                 name_match = re.search(lims_sample_name, xml_sample_name)
                 if not name_match:
                     log.append(
                         f"WARNING: LIMS sample name '{lims_sample_name}' does not match .xml sample name '{xml_sample_name}', please double check sample placement."
                     )
+            except AttributeError:
+                pass
 
         elif len(xml_matching_samples) < 1:
             log.append(
@@ -165,9 +167,15 @@ def main(lims, args):
             # Clean up
             os.remove(log_filename)
 
-            if errors:
+            if any("ERROR" in entry for entry in log):
                 sys.stderr.write("Some samples were skipped, please check the Log file")
-                sys.exit()
+                sys.exit(1)
+
+            if any("WARNING" in entry for entry in log):
+                sys.stderr.write(
+                    "Some samples generated warnings, please check the Log file"
+                )
+                sys.exit(1)
 
 
 def get_ba_output_file(currentStep, log):
