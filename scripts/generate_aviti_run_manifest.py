8c2991b73a047e363d861a3daf9444e146d488e8
kedhammar <alfred.kedhammar@scilifelab.se>
2024-08-20 16:10:38 +0200

    aviti dev init

---
+++ scripts/generate_aviti_run_manifest.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import logging
+import os
+import re
+import shutil
+from argparse import ArgumentParser
+from datetime import datetime as dt
+
+from genologics.config import BASEURI, PASSWORD, USERNAME
+from genologics.entities import Process
+from genologics.lims import Lims
+
+from scilifelab_epps.wrapper import epp_decorator
+
+DESC = """Script to generate Anglerfish samplesheet for ONT runs.
+"""
+
+TIMESTAMP = dt.now().strftime("%y%m%d_%H%M%S")
+
+
+@epp_decorator(script_path=__file__, timestamp=TIMESTAMP)
+def main(args):
+    lims = Lims(BASEURI, USERNAME, PASSWORD)
+    process = Process(lims, id=args.pid)
+
+    pass
+
+
+if __name__ == "__main__":
+    # Parse args
+    parser = ArgumentParser(description=DESC)
+    parser.add_argument(
+        "--pid",
+        required=True,
+        type=str,
+        help="Lims ID for current Process.",
+    )
+    parser.add_argument(
+        "--log",
+        required=True,
+        type=str,
+        help="Which file slot to use for the script log.",
+    )
+    parser.add_argument(
+        "--file",
+        required=True,
+        type=str,
+        help="Which file slot to use for the run manifest.",
+    )
+    args = parser.parse_args()
+
+    main()
