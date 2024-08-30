2fda2249bad7d709006f368824440f0c7ef0c002
kedhammar <alfred.kedhammar@scilifelab.se>
2024-08-30 15:45:30 +0200

    add logbook urls

--- data/logbook_data.py
+++ data/logbook_data.py
@@ -556,4 +556,8 @@ GDoc_logbook = {
     "Speedvac": {"File": "1Dk7qPJeNmzKtHWEdNkZ4yLB0FycjREqqIhNhInZ8G9g"},
     "Tecan": {"File": "1DUBEL8DBf0lnXJjIIjowQf2PrftMo9ECXpeNrDodM4s"},
     "VC100": {"File": "1FEH2Ilyaz2FThJZ_jjlDna7s4aTuRgfxvLFpYgOPHBw"},
+    "PromethION": {
+        "File": "1-sDggtU4RTAZuVOJ_N4Onv5loByamipSzPk9tpIAMYk"
+    },  # TODO 1OazL3MNZImTJBHK-QVy-jbxL9nN1uCoOmOMSiXclRac
+    "MinION": {"File": "1bfX3wlCkqphUyJZzHNcbTSUDx-z6uD85pho3u7a0mYw"},
 }
